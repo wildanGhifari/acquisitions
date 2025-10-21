45bde0577a7075d6630e670ec9c97f45ae7dc44d
wildanGhifari <wildghifari@gmail.com>
2025-10-21 22:16:27 +0700

    implement logger and middlewares

--- src/app.js
+++ src/app.js
@@ -1,8 +1,28 @@
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import logger from '#config/logger.js';
 
 const app = express();
 
+app.use(cors());
+app.use(cookieParser());
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+    morgan('combined', {
+        stream: {
+            write: message => logger.info(message.trim()),
+        },
+    })
+);
+
 app.get('/', (req, res) => {
+    logger.info('Acquisitions Service received a request.');
     res.status(200).send('Hello, from Acquisitions Service!');
 });
 
